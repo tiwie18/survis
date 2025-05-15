3a2650de28680407730331fdddde17d70e806c0d
tiwie18 <pratiwi.eka89@gmail.com>
2025-05-15 13:30:43 +0100

    update survis.js

--- js/survis.js
+++ js/survis.js
@@ -1,5 +1,23 @@
 window.surVisVersion = '0.1.0';
 
+var tagCloudOptions = [
+  {
+    field: 'keywords',
+    title: 'Keywords',
+    minTagFrequency: 1
+  },
+  {
+    field: 'author',
+    title: 'Authors',
+    minTagFrequency: 1
+  },
+  {
+    field: 'series',
+    title: 'Series',
+    minTagFrequency: 1
+  }
+];
+
 $(document).ready(function () {
   page.init();
   page.update(true);
