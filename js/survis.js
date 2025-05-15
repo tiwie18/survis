608742f26efc8ef3dd3cca8f7c7666f68bce3d3c
tiwie18 <pratiwi.eka89@gmail.com>
2025-05-15 13:40:29 +0100

    update survis.js

--- js/survis.js
+++ js/survis.js
@@ -1,16 +1,5 @@
 window.surVisVersion = '0.1.0';
 
-var tagCloudOptions = [
-  { field: 'keywords', title: 'Keywords', minTagFrequency: 1 },
-  { field: 'author', title: 'Authors', minTagFrequency: 1 },
-  { field: 'series', title: 'Series', minTagFrequency: 1 }
-];
-
-// ✅ Fix agar 'series' dikenali oleh bib.tagCategories
-var userDefinedTagCategories = {
-  series: { description: 'publication series (e.g., conference or journal)' }
-};
-
 $(document).ready(function () {
   page.init();
   page.update(true);
